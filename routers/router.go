7480a87f69456ced37977cac3029b0f1aab8698d
BrayanAlexander-Reyes <bryanreyes47k@gmail.com>
2026-05-20 17:47:35 -0500

    se ajusta archivos de una carpeta a otra, se hace migraciones, se crean commentsrouters y se ajusta codigo

---
+++ routers/router.go
@@ -0,0 +1,44 @@
+// @APIVersion 1.0.0
+// @Title beego Test API
+// @Description beego has a very cool tools to autogenerate documents for your API
+// @Contact astaxie@gmail.com
+// @TermsOfServiceUrl http://beego.me/
+// @License Apache 2.0
+// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
+package routers
+
+import (
+	"YoPLAY-APIS-CRUD-BEEGO/controllers"
+
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+func init() {
+	ns := beego.NewNamespace("/v1",
+
+		beego.NSNamespace("/documento",
+			beego.NSInclude(
+				&controllers.DocumentoController{},
+			),
+		),
+
+		beego.NSNamespace("/usuario",
+			beego.NSInclude(
+				&controllers.UsuarioController{},
+			),
+		),
+
+		beego.NSNamespace("/contrasena",
+			beego.NSInclude(
+				&controllers.ContrasenaController{},
+			),
+		),
+
+		beego.NSNamespace("/historial_acceso",
+			beego.NSInclude(
+				&controllers.HistorialAccesoController{},
+			),
+		),
+	)
+	beego.AddNamespace(ns)
+}
