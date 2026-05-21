5e749812994dab89c30773303f266ba3aacfa7b7
KevinHernandez18 <khernandezsierra895@gmail.com>
2026-05-20 21:29:27 -0500

    Se ejecuta primera fase y se reconfigura el main.go para esquema gestion_deportiva

--- gestion_deportiva/routers/router.go
+++ gestion_deportiva/routers/router.go
@@ -8,7 +8,7 @@
 package routers
 
 import (
-	":/Users/kevin/Desktop/YoPLAY-APIS-CRUD-BEEGO/gestion_deportiva/controllers"
+	"gestion_deportiva/controllers"
 
 	beego "github.com/beego/beego/v2/server/web"
 )
