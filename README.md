a1b91c05154e479e39673a3ca91a17b245770dca
BrayanAlexander-Reyes <bryanreyes47k@gmail.com>
2026-05-21 15:17:39 -0500

    se crea carpeta del esquema usuarios y se mueven los archivos carpeta original al archivo, se ajustan codigos y se crean diferentes tipos de archivos

--- README.md
+++
@@ -1,2 +0,0 @@
-# YoPLAY-APIS-CRUD-BEEGO
-Este es un proyecto generado con el objetivo de generar todas las API para el proyecto YoPLAY. Cada carpeta contendrá a un API CRUD operacional.contendrá un proyecto 
--- database/scripts/20260520_161614_yoplay_down.sql
+++
@@ -1,9 +0,0 @@
--- se elimina primero la que tiene la relacion 
-DROP TABLE IF EXISTS usuarios.documento;
--- sigue secuencial 
-DROP TABLE IF EXISTS usuarios.usuario;
-DROP TABLE IF EXISTS usuarios.contrasena;
-DROP TABLE IF EXISTS usuarios.historial_acceso
-
--- se elimina el esquema 
-DROP SCHEMA IF EXISTS usuarios;
